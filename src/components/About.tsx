917261ec43dc13da91875d112941aa36b71f144b
Krishna Shukla <130078306+Krishnazzz@users.noreply.github.com>
2025-01-19 23:26:13 +0530

    Add files via upload

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+        Hello! I'm Krishna Shukla, a passionate Cybersecurity Analyst and dedicated Web Developer with a strong foundation in cybersecurity principles and a creative approach to problem-solving. Currently pursuing my Bachelor’s degree in Computer Applications (BCA), I blend my academic knowledge with hands-on experience to build innovative solutions that bridge technology and security.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
